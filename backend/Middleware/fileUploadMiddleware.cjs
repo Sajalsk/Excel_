58c135371d103061f17f5978d4611e7516e7b6a0
Sajalsk <sajalsk247@gmail.com>
2023-07-22 08:38:53 +0530

    feat:4

--- backend/Middleware/fileUploadMiddleware.cjs
+++
@@ -1,8 +0,0 @@
-// fileUploadMiddleware.js
-const fileUpload = require("express-fileupload")
-
-module.exports = fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 }, // Maximum file size (50 MB)
-});
-
-export default mongoose.model("fileUpload", fileUploadMiddleware);
